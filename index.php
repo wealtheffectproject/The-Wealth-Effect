cc0f0c73c81bc8e0a398f33e958eb895b3da51cd
Scott Caratozzolo <scaratozzolo12@gmail.com>
2017-01-30 19:03:25 +0000

    Beginning Work

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
-<?php
-?>
+
 
 <html>
 
@@ -7,13 +6,34 @@
 <title>The Wealth Effect</title>
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-<link rel="stylesheet" href="/style.css" type="text/css">
+<link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
 
 <body>
 
 <h1>The Wealth Effect</h1>
 
+<div> 
+<a href="http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html" id="button 1" ><button type="button" class="btn btn-default">Button 1</button></a>
+  <button type="button" class="btn btn-default">Button 2</button>
+  <button type="button" class="btn btn-default">Button 3</button>
+</div>
+
+
+<h3> About the Wealth Effect </h3>
+<p>   
+How should I start it?
+"You like jazz?" No, that's no good.
+Here she comes! Speak, you fool!
+Hi!
+I'm sorry.  
+- You're talking.
+- Yes, I know.
+You're talking!
+I'm so sorry.
+No, it's OK. It's fine.
+I know I'm dreaming.
+</p>
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
