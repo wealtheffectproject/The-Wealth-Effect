0201286f4b20a0d36ffb59062c06abe21ff5d5fa
Scott Caratozzolo <scaratozzolo12@gmail.com>
2017-02-02 18:24:17 +0000

    Jake is the only one working

--- index.php
+++ index.php
@@ -8,17 +8,45 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css" type="text/css">
 </head>
-
 <body>
-<div class="input-group">
-  <span class="input-group-addon" id="sizing-addon2">@</span>
-  <input type="text" class="form-control" placeholder="Username" aria-describedby="sizing-addon2">
-</div>
-<h1>The Wealth Effect</h1>
+
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+   
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <h1>The Wealth Effect</h1>
+    </div>
+      <form id="signin" class="navbar-form navbar-right" role="form">
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                            <input id="email" type="email" class="form-control" name="email" value="" placeholder="Email Address">                                        
+                        </div>
+
+                        <div class="input-group">
+                            <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                            <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">                                        
+                        </div>
+
+                        <button type="submit" class="btn btn-primary">Login</button>
+                   </form>
+     
+  </div>
+</nav>
+
 <div class=buttons>
-<a href="http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html" id="button 1" ><button type="button" class="btn btn-default">Button 1</button></a>
-  <button type="button" class="btn btn-default">Button 2</button>
+<a href="http://www.script-o-rama.com/movie_scripts/a1/bee-movie-script-transcript-seinfeld.html" id="button 1" ><button type="button" class="btn btn-default">Income</button></a>
+<a href="http://www.sick.bike" id="button 2" ><button type="button" class="btn btn-default">Rent</button></a>
+<a href="http://www.omfgdogs.com/" id="button 3" ><button type="button" class="btn btn-default">Automobile</button></a>
+<a href="http://www.sanger.dk/" id="button 4" ><button type="button" class="btn btn-default">Budget</button></a>
 </div>
+
 
 <h3> About the Wealth Effect </h3>
 <p>   
