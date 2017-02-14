12b5f997ea0ad74d29aa1225928c0c15ce9ce426
wealtheffectproject <scaratozzolo@sussextechstudents.com>
2017-02-14 14:17:36 -0500

    begin work on new section

--- home.js
+++ home.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 //"use strict";
 $(document).ready(function(){
-    $('#learn-more').click(function(){
-        $('#about').css('display', 'block').animate({
-        'top' : "-=100%" //moves up
-        });
-    });
+    $("#test").click(function() {
+    	$('html, body').animate({
+        scrollTop: $("div").next("#section").offset().top
+    	}, 800);		
+	});
 });
--- index.php
+++ index.php
@@ -6,6 +6,7 @@
 <title>The Wealth Effect</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" href="Images/wandie.jpg">
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.css" />
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 <link rel="stylesheet" href="css/style.css" type="text/css">
@@ -33,13 +34,15 @@
             </div>
 
         	<button type="submit" class="btn btn-primary">Login</button>
+            <button type="button" class="btn btn-default">Sign Up</button>
+            <button type="button" class="btn btn-danger" id="test">test</button>
 		</form>   
 	</div>
 </nav>
 
 
 
-<div id="head">
+<div class="head" id="section">
     <h1>The Wealth Effect</h1>
     <h2>Invest In Your Future</h2>
     <button id="learn-more" class="btn-lg btn-primary">Learn More</button>
@@ -47,13 +50,32 @@
 
 
 
-<div id="about">
+<div class="about" id="section">
 	<div class="container">
-		<h1>WELCOME TO WANDIE WORLD</h1>
+		<h1>About The Wealth Effect</h1>
+	</div>
+</div>
+
+
+<div class="features" id="section">
+	<div class="container">
+		<h1>Features of The Wealth Effect</h1>
+        <div class="row">
+        	<img class="col-md-4" src="Images/wandie.jpg" />
+            <img class="col-md-4" src="Images/wandie.jpg" />
+            <img class="col-md-4" src="Images/wandie.jpg" />
+        </div>
+        <div class="row">
+        	<img class="col-md-4" src="Images/wandie.jpg" />
+            <img class="col-md-4" src="Images/wandie.jpg" />
+            <img class="col-md-4" src="Images/wandie.jpg" />
+        </div>
 	</div>
 </div>
 
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script src="home.js" type="text/javascript"></script>
