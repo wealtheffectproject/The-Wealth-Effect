8f20819b019a98a1a522c55334a6f3b01db1df54
natethegreat404 <ngile@sussextechstudents.com>
2017-02-22 19:14:58 +0000

    Created "forgotPassword.php" & "createAccount.php"

---
+++ createAccount.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <title>Welcome to the World of Finance</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/login.css" type="text/css">
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+            <h3>The</h3>
+            <h1>Wealth Effect</h1>
+    </div>
+  </div>
+</nav>
+
+
+</div>
+<div class="background">
+    <img src="img/loginBackground.jpg" alt="Printed Money">
+</div>
+</body>
+</html>
