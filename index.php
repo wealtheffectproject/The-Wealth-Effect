3cf8141d30ca8a92e2174a768f2b4c04565bfc11
Scott Caratozzolo <scaratozzolo12@gmail.com>
2017-02-23 13:42:41 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -23,14 +23,14 @@
 	<div class="container">
 		<div class="navbar-brand">The Wealth Effect</div>
     
-    	<form id="signin" class="navbar-form navbar-right" role="form">
+    	<form id="signin" class="navbar-form navbar-right" role="form" action="login.php" method="post">
         	<div class="input-group">
              	<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-             	<input id="email" type="email" class="form-control" name="email" value="" placeholder="Email Address">                                        
+             	<input id="username" type="text" class="form-control" name="username" placeholder="Email Address or Username">                                        
 			</div>
             <div class="input-group">
             	<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-            	<input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">                                        
+            	<input id="password" type="password" class="form-control" name="password" placeholder="Password">                                        
             </div>
 
         	<button type="submit" class="btn btn-primary">Login</button>
